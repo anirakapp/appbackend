d7dbef88ce118534b40cd399e0d1d7a979f5d7e2
anirakapp <anirakconstruccion@gmail.com>
2026-09-05 09:06:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const userModel = require("./models/userModel");
-
 const authRoutes = require("./routes/authRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const negociosRoutes = require("./routes/negociosRoutes");
 const calculosRoutes = require("./routes/calculosRoutes");
+const searchRoutes = require("./routes/searchRoutes");
 
 const app = express();
 
@@ -30,10 +30,12 @@ app.use(async (req, res, next) => {
 });
 
 app.get("/", (req, res) => res.json({ ok: true, message: "Servidor funcionando 🚀" }));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/negocios", negociosRoutes);
 app.use("/api/calculo", calculosRoutes);
+app.use("/api/search", searchRoutes); // NUEVO
 
 app.use((req, res) => res.status(404).json({ message: "Ruta no encontrada" }));
 app.use((err, req, res, next) => {
