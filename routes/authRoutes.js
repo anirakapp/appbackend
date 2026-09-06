fa54c059601975eac036729498686cef0ebb133f
anirakapp <anirakconstruccion@gmail.com>
2026-09-05 21:20:12 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,11 +2,9 @@
 const express = require("express");
 const authController = require("../controllers/authController");
 const { requireAuth } = require("../middleware/authMiddleware");
-
 const router = express.Router();
-
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/me", requireAuth, authController.me);
-
+router.patch("/avatar", requireAuth, authController.actualizarAvatar);
 module.exports = router;
